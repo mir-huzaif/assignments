a76325cf3dfa8060f3931dd6f59e631636883917
sharonheller <mirhuzeif@gmail.com>
2024-06-03 23:56:52 +0530

    todoList

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,6 +11,31 @@
 */
 
 class Todo {
+  constructor(){
+    this.list = [];
+  }
+
+  add(todo){
+    this.list.push(todo);
+  }
+  remove(indexOfTodo){
+    this.list.splice(indexOfTodo, 1);
+  }
+  update (index, updatedTodo){
+    if(index < this.list.length && index >-1)
+      this.list[index] = updatedTodo;
+  }
+  getAll(){
+    return this.list;
+  }
+  get(indexOfTodo){
+    if (this.list[indexOfTodo])
+      return this.list[indexOfTodo];
+    return null;
+  }
+  clear(){
+    this.list = [];
+  }
 
 }
 
